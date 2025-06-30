325b34ce8af9d021622a3ff5d3afeda18d297c3d
savar95x <skaar513@gmail.com>
2025-07-01 01:30:44 +0530

    trying comparison slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,26 @@
 import streamlit as st
 from PIL import Image, ImageEnhance
+from streamlit_image_comparison import image_comparison
 import io
 
 st.set_page_config(layout="wide")
 st.title("Image Editor")
+#default_image = "/home/savar/pix/walls/sakura.png"
 default_image = "theoffice.jpg"
-
-with st.sidebar:
-    st.header("Settings")
-    contrast = st.slider("contrast", 0.5, 2.0, 1.0, 0.1)
-    brightness = st.slider("brightness", 0.5, 2.0, 1.0, 0.1)
-    grayscale = st.slider("grayscale", 0.0, 1.0, 0.5, 0.1)
 
 uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
 
-st.write(
-        "Open the Sidebar to edit the image."
-)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    contrast = st.slider("Contrast", 0.5, 2.0, 1.0, 0.02, key="contrast")
+
+with col2:
+    brightness = st.slider("Brightness", 0.5, 2.0, 1.0, 0.02, key="brightness")
+
+with col3:
+    grayscale = st.slider("Grayscale", 0.0, 1.0, 0.5, 0.02, key="grayscale")
+
 # load image
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -28,11 +32,19 @@ edited_image = ImageEnhance.Color(edited_image).enhance(1 - grayscale)
 edited_image = ImageEnhance.Brightness(edited_image).enhance(brightness)
 edited_image = ImageEnhance.Contrast(edited_image).enhance(contrast)
 
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader("Original Image")
-    st.image(image, use_container_width=True)
+col1, col2, col3 = st.columns([1, 8, 1])
 
-with col2:
-    st.subheader("Edited Image")
-    st.image(edited_image, use_container_width=True)
+with col2:  # This centers the component
+    # Use CSS to force full width within the center column
+    image_comparison(
+        img1=image,
+        img2=edited_image,
+        label1="Original",
+        width=10000,
+        label2="Edited",
+        starting_position=48,
+        show_labels=True,
+        make_responsive=True,
+        in_memory=True,
+    )
+
