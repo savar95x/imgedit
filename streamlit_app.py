51095cc4f16ac7e4c012e57cb313c48cd95b1d9f
savar95x <skaar513@gmail.com>
2025-07-01 11:46:07 +0530

    slider too janky

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,25 @@
 import streamlit as st
 from PIL import Image, ImageEnhance
-from streamlit_image_comparison import image_comparison
 import io
 
 st.set_page_config(layout="wide")
 st.title("Image Editor")
-#default_image = "/home/savar/pix/walls/sakura.png"
 default_image = "theoffice.jpg"
 
-uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
+with st.sidebar:
+    uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
+    if uploaded_file:
+        st.image(uploaded_file)
+    else:
+        st.image(default_image)
+    contrast = st.slider("contrast", 0.5, 2.0, 1.0, 0.05)
+    brightness = st.slider("brightness", 0.5, 2.0, 1.0, 0.05)
+    grayscale = st.slider("grayscale", 0.0, 1.0, 0.5, 0.05)
 
-col1, col2, col3 = st.columns(3)
+st.write(
+        "Open the Sidebar to upload or edit image."
+)
 
-with col1:
-    contrast = st.slider("Contrast", 0.5, 2.0, 1.0, 0.02, key="contrast")
-
-with col2:
-    brightness = st.slider("Brightness", 0.5, 2.0, 1.0, 0.02, key="brightness")
-
-with col3:
-    grayscale = st.slider("Grayscale", 0.0, 1.0, 0.5, 0.02, key="grayscale")
-
-# load image
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
 else:
@@ -32,19 +30,5 @@ edited_image = ImageEnhance.Color(edited_image).enhance(1 - grayscale)
 edited_image = ImageEnhance.Brightness(edited_image).enhance(brightness)
 edited_image = ImageEnhance.Contrast(edited_image).enhance(contrast)
 
-col1, col2, col3 = st.columns([1, 8, 1])
-
-with col2:  # This centers the component
-    # Use CSS to force full width within the center column
-    image_comparison(
-        img1=image,
-        img2=edited_image,
-        label1="Original",
-        width=10000,
-        label2="Edited",
-        starting_position=48,
-        show_labels=True,
-        make_responsive=True,
-        in_memory=True,
-    )
-
+st.subheader("Edited Image")
+st.image(edited_image)
